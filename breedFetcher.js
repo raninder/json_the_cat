70d3b4b50786abe9f3afa800e2b620315bd122c0
raninder <raninderdhillon@gmail.com>
2021-11-05 00:46:38 +0000

    breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,32 @@
 const request = require('request');
-const args = process.argv;
-const breedName = args[2];
-url = "https://api.thecatapi.com/v1/breeds/search?q="
-newURL = url+breedName;
-console.log(newURL);
-request(newURL, (error, response, body) => {
-    if(error) {
-        console.log("error:" ,error );
-    }
-    else {
-    let data = JSON.parse(body);
-    
-    if(data.length) {
-        let data = JSON.parse(body);
-        console.log(data[0].description);
-        
-    }
-    else {
-        console.log('Breed not found');
-     }
-    }
-});
+
+
+
+const fetchBreedDescription = function (breedName, callback) {
+    const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+
+    request(url, (error, response, body) => {
+        if (error) {
+            callback(error, null);
+        }
+        else {
+            let data = JSON.parse(body);
+            let breed = data[0];
+            // if(data.length) {
+            //     let data = JSON.parse(body);
+            //     console.log(data[0].description); 
+            // }
+            // else {
+            //     console.log('Breed not found');
+            //  }
+            //console.log(data);
+            if (breed) {
+                callback(null, breed.description);
+            }
+            else {
+                callback("breed not found", null);
+            }
+        }
+    });
+}
+module.exports = { fetchBreedDescription };
